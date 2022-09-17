8aa5f75463cb5c19e736a21ee978776d6280a53a
LukeScrewdriver <68291618+LukeScrewdriver@users.noreply.github.com>
2022-09-17 23:56:24 +0200

    Release v1.2

--- compile.sh
+++
@@ -1,2 +0,0 @@
-docker build --tag lukescrewdriver/animeautoload:1.0.25 .
-docker build --tag lukescrewdriver/animeautoload:latest .
--- export.sh
+++
@@ -1,1 +0,0 @@
-docker save --output animeautoload.tar lukescrewdriver/animeautoload:latest
