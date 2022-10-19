8f7604ec9b409494bedccc2839d53ec781cfb237
LukeScrewdriver <68291618+LukeScrewdriver@users.noreply.github.com>
2022-10-20 00:14:25 +0200

    Release v01.07
     - added longterm anime option

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # AnimeDownloader
 
-
+On the file ```data.json``` add a new element on the array called ```list```.
+```
+        {
+            "animeName": "One Piece",
+            "downloadType": "latest",
+            "seasonIndex": 21,
+            "longTermAnime": true
+        }
+```
+ **Explaination parameters**
+  - ```animeName``` : the name of the anime
+  - ```downloadType``` : type of download
+    - all : not implemented
+    - latest : download the latest episode available
+  - ```longTermAnime``` if the anime is without end like One piece, Conan, Naruto... set it *true*.
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var managerFolders = require('./lib/LS/managerFolders')
 var json = require('./lib/LS/json')
 var downloader = require('./lib/AnimeDownloader/download')
 
-const VERSION = "01.06"
+const VERSION = "01.07"
 
 logger.info("script.js", "Version: " + VERSION)
 
@@ -25,6 +25,7 @@ var dataProgram = {
 //animeName:
 //downloadType: all latest
 //seasonIndex: 
+//longTermAnime: true false
 
 
 
@@ -56,7 +57,7 @@ var getAnime = (data) => new Promise((resolve, reject) => {
         default:
             logger.info("script.js", data.animeName + " " + data.seasonIndex + " Downloading options: latest")
             var searchString = data.animeName
-            if (Number(data.seasonIndex) > 1)
+            if (Number(data.seasonIndex) > 1 && !data.longTermAnime)
                 searchString = searchString + " " + data.seasonIndex
 
             aniworldextractor.getLastEpIndex(searchString)
@@ -70,6 +71,7 @@ var getAnime = (data) => new Promise((resolve, reject) => {
                             .then(link => {
                                 logger.info("script.js", "Downloading " + data.animeName + " " + data.seasonIndex + " Episode: " + indexLast)
                                 downloader.downloadMedia(link, filePath + fileName).then(success => {
+                                    logger.info("script.js", data.animeName + " " + data.seasonIndex + " Episode: " + indexLast +" Download complete!")
                                     resolve()
                                 })
                             })
